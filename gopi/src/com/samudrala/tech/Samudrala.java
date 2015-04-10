de21931bf4f94625134299f150e9b8b2dc7ea1f0
bikshapathi.samudrala <bikashapathi.samudrala@gmail.com>
2015-04-10 14:45:26 +0530

    iam just traying to commit the code

---
+++ gopi/src/com/samudrala/tech/Samudrala.java
@@ -0,0 +1,10 @@
+package com.samudrala.tech;
+
+public class Samudrala {
+	
+	public static void main(String... str){
+		System.out.println("imazin some informtion is existed here.....");
+		
+	}
+
+}
